1edde2c4f57eeb55cd8220c119f9dd030ef6f270
WiktorPospiech <wiktor.pospiech@gmail.com>
2024-08-29 21:30:02 +0200

    searchBox added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,35 @@ import "./App.css";
 import ContactList from "./components/ContactList//ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import FeedbackForm from "./components/Form/Form";
-
+import { useState } from "react";
+import allContacts from "./components/Contact/allContacts.json";
+const searchingContact = [];
 function App() {
+  const [inputValue, setInputValue] = useState(allContacts);
+
+  //funkcja, która zostanie przekazana do SearchBox//
+  const handleChange = (evt) => {
+    setInputValue(evt.target.value);
+    searchingContact.splice(0);
+
+    for (const contact of allContacts) {
+      {
+        contact.name.toLowerCase().includes(evt.target.value.toLowerCase()) &&
+          searchingContact.push(contact);
+        console.log(...searchingContact);
+      }
+    }
+  };
+
   return (
     <>
       <h1>Phonebook</h1>
       <FeedbackForm />
-      <SearchBox />
+      <SearchBox
+        key={allContacts.id}
+        value={inputValue}
+        onUpdate={handleChange}
+      />
       <ContactList />
     </>
   );
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,49 +3,44 @@
 //     evt.preventDefault();
 //     console.log(evt);
 //   }
-import { useState } from "react";
+// import { useState } from "react";
 
-const allContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const allContacts = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
-const searchingContact = [];
+// const searchingContact = [];
 
-const SearchBox = () => {
-  const [inputValue, setInputValue] = useState("");
-  const handleChange = (evt) => {
-    setInputValue(evt.target.value);
-    searchingContact.splice(0);
+const SearchBox = ({ onUpdate }) => {
+  // const [inputValue, setInputValue] = useState("");
+  // const handleChange = (evt) => {
+  //   setInputValue(evt.target.value);
+  //   searchingContact.splice(0);
 
-    console.log(evt.target.value);
-    console.log(...allContacts);
-    console.log(...searchingContact);
-    for (const contact of allContacts) {
-      {
-        console.log(contact.name);
-        console.log(contact.name.includes(evt.target.value));
-        console.log(contact.name.indexOf(evt.target.value));
-        contact.name.includes(evt.target.value) &&
-          searchingContact.push(contact);
-        console.log(...searchingContact);
-      }
-    }
-  };
+  //   console.log(evt.target.value);
+  //   console.log(inputValue);
+  //   console.log(...allContacts);
+  //   console.log(...searchingContact);
+  //   for (const contact of allContacts) {
+  //     {
+  //       console.log(contact.name);
+  //       console.log(contact.name.includes(evt.target.value));
+  //       console.log(contact.name.indexOf(evt.target.value));
+  //       contact.name.includes(evt.target.value) &&
+  //         searchingContact.push(contact);
+  //       console.log(...searchingContact);
+  //     }
+  //   }
+  // };
 
   return (
     <>
       <p>Find contact by name</p>
 
-      <input
-        onChange={handleChange}
-        type="text"
-        value={inputValue}
-        name="wanted"
-      />
-      <p>You are looking for:{inputValue}</p>
+      <input onChange={onUpdate} type="text" name="wanted" />
     </>
   );
 };
